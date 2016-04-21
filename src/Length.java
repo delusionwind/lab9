f44726a5ec2a887c2bb2175d11af9995a8464eb0
Delusion <delusionwind@gmail.com>
2016-04-21 15:36:22 +0700

    DistanceConverter Runnable with app

--- src/Length.java
+++ src/Length.java
@@ -1,4 +1,25 @@
 
-public enum Length {
-
+public enum Length implements Unit{
+	METER( "Meter", 1.0 ),
+	FOOT( "Foot", 0.3048 ),
+	INCH( "Inch", 0.0254 ),
+	MILE( "Mile", 1609.344 ),
+	YARD( "Yard", 3*FOOT.getValue() ),
+	MICRON( "Micron", 1.0E-6 ),
+	CENTIMETER( "Centimeter", 0.01 ),
+	KILOMETER( "Kilometer", 1000 ),
+	WA( "Wa", 2 );
+	
+	
+	public final String name;
+	
+	public final double value;
+	
+	private Length(String n, double v) {
+		name = n;
+		value = v;
+	}
+	
+	public double getValue() { return value; }
+	public String toString() { return name; }
 }
